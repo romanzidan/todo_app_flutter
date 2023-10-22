49238fb42be4380ec6ad296e60725c5f4de858eb
rzkzx <romanzidanramadhan@gmail.com>
2023-10-22 21:35:07 +0800

    Todo App Flutter

---
+++ lib/widgets/todo_item.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+import '../constrants/colors.dart';
+import '../model/todo.dart';
+
+class ToDoItem extends StatelessWidget {
+  final ToDo todo;
+  final onToDoChanged;
+  final onDeleteItem;
+
+  const ToDoItem({
+    super.key,
+    required this.todo,
+    required this.onToDoChanged,
+    required this.onDeleteItem,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 20),
+      child: ListTile(
+        onTap: () {
+          // print('clicked');
+          onToDoChanged(todo);
+        },
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        contentPadding: const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+        tileColor: Colors.white,
+        leading: Icon(
+          todo.isDone ? Icons.check_box : Icons.check_box_outline_blank,
+          color: tdBlue,
+        ),
+        title: Text(
+          todo.todoText!,
+          style: TextStyle(
+            fontSize: 16,
+            color: tdBlack,
+            decoration: todo.isDone ? TextDecoration.lineThrough : null,
+          ),
+        ),
+        trailing: Container(
+            height: 35,
+            width: 35,
+            decoration: BoxDecoration(
+              color: tdRed,
+              borderRadius: BorderRadius.circular(5),
+            ),
+            child: IconButton(
+              color: Colors.white,
+              iconSize: 18,
+              icon: const Icon(Icons.delete),
+              onPressed: () {
+                // print('deleted');
+                onDeleteItem(todo.id);
+              },
+            )),
+      ),
+    );
+  }
+}
